ebe558da5123daaab956c40d627f44e7dd422509
Gedewon <gedewon.hayle.9@gmail.com>
2020-09-06 22:34:24 +0300

    npm script's

---
+++ week4/js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#mycarousel').carousel({ interval: 2000 });
+
+    $('#carouselButton').click(function () {
+
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        } else {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').addClass('fa-pause');
+            $('#carouselButton').children('span').removeClass('fa-play');
+
+        }
+
+
+    });
+
+    $('#login').on('click', function () {
+        $('#loginModal').modal('show');
+    });
+
+    $('#reserve').click(function () {
+        $('#reserveModel').modal('show');
+    });
+
+
+
+
+});
